e5eb43011d0801536fb427e11dc345201624b0c4
Aaron Sorensen <indiefilmer9@Gmail.com>
2019-04-28 21:52:10 -0600

    Flesh out the entity factory and test.

--- src/ecs/entity-factory.spec.ts
+++ src/ecs/entity-factory.spec.ts
@@ -3,23 +3,105 @@ import "mocha";
 
 import { EntityFactory } from "./entity-factory";
 import { Component } from "./Component";
+import { Entity } from "./Entity";
 
-class TestComponent1 implements Component{ }
+describe("Entity factory works", function () {
+    it("Can make entity", function () {
+        class TestComponent1 implements Component {}
+        let testComponents = { TestComponent1 };
 
-describe("Entity factory works", function() {
-    it("Can make entity", function() {
-        let test = [
+        let testBlueprints = [
+            {
+                "name": "Base",
+                "components": [
+                    { "name": "TestComponent1" }
+                ]
+            }
+        ];
+
+        const factory = new EntityFactory(testBlueprints, testComponents);
+        let entity = factory.buildEntity(<any>"Base");
+        expect(entity.hasComponent(TestComponent1)).to.be.true;
+    });
+
+    it("Built entity inherits components from other blueprints", function () {
+        class TestComponent1 implements Component {}
+        class TestComponent2 implements Component {}
+        class TestComponent3 implements Component {}
+        class TestComponent4 implements Component {}
+        let testComponents = { TestComponent1, TestComponent2, TestComponent3, TestComponent4 };
+
+        let testBlueprints = [
+            {
+                "name": "Base",
+                "components": [
+                    { "name": "TestComponent1" }
+                ]
+            },
+            {
+                "name": "Inherits",
+                "blueprints": ["Base"],
+                "components": [
+                    { "name": "TestComponent2" },
+                    { "name": "TestComponent3" }
+                ]
+            },
+            {
+                "name": "InheritsTwice",
+                "blueprints": ["Inherits"],
+                "components": [
+                    { "name": "TestComponent4" }
+                ]
+            }
+        ];
+        const factory = new EntityFactory(testBlueprints, testComponents);
+        let entity = factory.buildEntity(<any>"InheritsTwice");
+        expect(entity.hasComponent(TestComponent1)).to.be.true;
+        expect(entity.hasComponent(TestComponent2)).to.be.true;
+        expect(entity.hasComponent(TestComponent3)).to.be.true;
+        expect(entity.hasComponent(TestComponent4)).to.be.true;
+    });
+
+    it("Child blueprint overrides inherited component values", function () {
+        class TestComponent1 implements Component { value = 'default'; value2 = 'untouched' }
+        class TestComponent2 implements Component { value = 'default'; }
+        class TestComponent3 implements Component { value = 'default'; }
+        let testComponents = { TestComponent1, TestComponent2, TestComponent3 };
+
+        let testBlueprints = [
             {
                 "name": "Base",
                 "blueprints": [],
                 "components": [
-                    { "name": "PositionComponent", "values": {} },
-                    { "name": "RenderComponent", "values": {} }
+                    { "name": "TestComponent1", "values": {value: 'baseChanged'} },
+                    { "name": "TestComponent2", "values": {value: 'baseChanged'} },
+                    { "name": "TestComponent3", "values": {} }
+                ]
+            },
+            {
+                "name": "Inherits",
+                "blueprints": ["Base"],
+                "components": [
+                    { "name": "TestComponent2", "values": {value: 'inheritsChanged'} }
                 ]
             }
         ];
-        const factory = new EntityFactory(test, {TestComponent1});
-        let entity = factory.buildEntity(<any>"Base");
-        expect(() => entity.hasComponent(TestComponent1)).to.be.true;
-      });
+        const factory = new EntityFactory(testBlueprints, testComponents);
+        let entity = factory.buildEntity(<any>"Inherits");
+        expect(entity.getComponent(TestComponent1).value).to.equal('baseChanged');
+        expect(entity.getComponent(TestComponent1).value2).to.equal('untouched');
+        expect(entity.getComponent(TestComponent2).value).to.equal('inheritsChanged');
+        expect(entity.getComponent(TestComponent3).value).to.equal('default');
+    });
+
+    it("Blueprint must implement at least one component", function () {
+        const factory = new EntityFactory([], {});
+        expect(() => factory['getComponentsFromJson']([])).to.throw();
+    });
+    
+    it("Blueprint type must exist", function () {
+        //TODO enter actual array of types
+        const factory = new EntityFactory([], {});
+        expect(() => factory['getBlueprintFromType'](<any>'NotFound')).to.throw();
+    });
 });
--- src/ecs/entity-factory.ts
+++ src/ecs/entity-factory.ts
@@ -3,43 +3,56 @@ import { Blueprint, BlueprintComponent } from "./blueprint";
 import { Entity } from "./entity";
 
 export class EntityFactory {
-    private blueprints;
+    private blueprints: Blueprint[] = [];
     private components;
 
-    constructor(blueprints, components) {
-        this.blueprints = blueprints;
-        this.components = components;
-    }
+    // TODO - Validate json (no duplicate named blueprints etc);
+    // TODO - Implement a caching strategy if we've already built an entity;
     
+    constructor(blueprintJson, componentModule) {
+        this.components = componentModule;
+        this.blueprints = this.buildBlueprintsFromJson(blueprintJson);
+    }
+
     public buildEntity(type: BlueprintType): Entity {
-        console.log('building from blueprint', this.getBlueprintFromType(type));
         return this.getEntityFromBlueprint(this.getBlueprintFromType(type), new Entity());
     }
 
     private getEntityFromBlueprint(blueprint: Blueprint, entity: Entity): Entity {
+
+        // Recursively add components from inherited blueprints
+        blueprint.blueprints.forEach(x => {
+            entity = this.getEntityFromBlueprint(this.getBlueprintFromType(x), entity);
+        });
+
         blueprint.components.forEach(x => {
             entity.putComponent(<any>x.component);
-            let component = entity.getComponent(<any>x.component);
-            // TODO iterate over value properties and set.
-            // TODO recursively build inherited components.
+
+            // Overwrite values of component based off of blueprint;
+            if (x.values) {
+                let component = entity.getComponent(<any>x.component);
+                Object.getOwnPropertyNames(x.values).forEach(value => component[value] = x.values[value]);
+            }
         });
 
         return entity;
     }
 
     private getBlueprintFromType(type: BlueprintType): Blueprint {
-        let jsonBlueprint = this.blueprints.find(x => x.name === type);
-        if (!jsonBlueprint) {
+        let blueprint = this.blueprints.find(x => x.name === type);
+        if (!blueprint) {
             throw new Error("Cannot find blueprint by that name.");
         }
-        return this.buildBlueprintFromJson(jsonBlueprint);
+        return blueprint;
     }
 
-    private buildBlueprintFromJson(jsonBlueprint): Blueprint {
-        return new Blueprint(
-            jsonBlueprint.name,
-            this.getComponentsFromJson(jsonBlueprint.components),
-            this.hasBlueprints(jsonBlueprint) ? jsonBlueprint.blueprints.map(x => this.buildBlueprintFromJson(x)) : []
+    private buildBlueprintsFromJson(jsonBlueprints): Blueprint[] {
+        return jsonBlueprints.map(x =>
+            new Blueprint(
+                x.name,
+                this.getComponentsFromJson(x.components),
+                this.hasBlueprints(x) ? x.blueprints : []
+            )
         );
     }
 
@@ -47,8 +60,12 @@ export class EntityFactory {
         return jsonBlueprint.blueprints && jsonBlueprint.blueprints.length > 0;
     }
 
+    /**
+     * Converts json components to blueprint components.
+     * @throws if the list is empty.
+     * @param components json array for the blueprints components.
+    */
     private getComponentsFromJson(components): BlueprintComponent[] {
-        console.log('trying to build components', components);
         if (!components || components.length === 0) {
             throw new Error("Blueprint must implement one or more components.");
         } else {
